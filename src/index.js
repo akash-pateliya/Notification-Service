eb472503dcdb763f8a7cb2e653c062ee2a432c83
Akash Pateliya <akashpateliya00@gmail.com>
2022-04-23 02:16:27 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+
+// create express app
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// Configuring the database
+// const dbConfig = require('./config/database.config.js');
+// const mongoose = require('mongoose');
+
+// mongoose.Promise = global.Promise;
+
+// // Connecting to the database
+// mongoose.connect(dbConfig.url, {
+//     useNewUrlParser: true
+// }).then(() => {
+//     console.log("Successfully connected to the database");
+// }).catch(err => {
+//     console.log('Could not connect to the database. Exiting now...', err);
+//     process.exit();
+// });
+
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to Notification Service. Send Notification quickly. Schedule notification for any medium e.g. Text, Email, Whatsapp."});
+});
+// require('./app/routes/note.routes.js')(app);
+// listen for requests
+app.listen(process.env.node_port, () => {
+    console.log("Server is listening on port ",process.env.node_port);
+});
