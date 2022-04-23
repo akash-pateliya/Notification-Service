53f4f3273e33bbfdda12a76cee034001ccdd2723
Akash Pateliya <akashpateliya00@gmail.com>
2022-04-23 18:51:46 +0530

    try catch added

--- src/app/bll/medium.bll.js
+++ src/app/bll/medium.bll.js
@@ -4,16 +4,20 @@ const { v4: uuid } = require('uuid');
 class mediumBLL {
 
     async insertMedium(mediumObject) {
-        const medium = new Medium({
-            uid: uuid(),
-            medium_name: mediumObject.medium_name,
-            isDeleted: true,
-            createdAt: new Date(),
-            updatedAt: null,
-            deletedAt: null
-        });
-        const result = await medium.save();
-        return result;
+        try {
+            const medium = new Medium({
+                uid: uuid(),
+                medium_name: mediumObject.medium_name,
+                isDeleted: true,
+                createdAt: new Date(),
+                updatedAt: null,
+                deletedAt: null
+            });
+            const result = await medium.save();
+            return result;
+        } catch (error) {
+            throw new Error(`Method : insertMedium, Class : mediumBLL, Error : ${error}`);
+        }
     }
 }
 
--- src/app/controllers/medium.controller.js
+++ src/app/controllers/medium.controller.js
@@ -3,11 +3,15 @@ const mediumBLL = require('../bll/medium.bll.js');
 const eResponse = require('../enum/response.enum.js');
 
 exports.create = async (req, res) => {
-    if(!req.body?.medium_name) {
-        return res.status(StatusCodes.BAD_REQUEST).send({
-            message: eResponse.EmptyContent
-        });
+    try {
+        if(!req.body?.medium_name) {
+            return res.status(StatusCodes.BAD_REQUEST).send({
+                message: eResponse.EmptyContent
+            });
+        }
+        const result = await new mediumBLL().insertMedium(req.body);
+        res.status(StatusCodes.OK).send(result);
+    } catch (error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
     }
-    const result = await new mediumBLL().insertMedium(req.body);
-    res.status(StatusCodes.OK).send(result);
 };
